f273d42575dbe34cc3710fd60b0b584e0c7f61a1
itok toni <itok.toni@gmail.com>
2025-11-11 14:27:14 +0700

    update

--- resources/views/kotor/print.blade.php
+++ resources/views/kotor/print.blade.php
@@ -1,90 +1,75 @@
 <x-report print="print">
 
-    <div class="header-action">
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Invoice</title>
+
+</head>
+<body>
+
+<div class="invoice">
+
+    <!-- HEADER -->
+    <div class="invoice-header">
+         @if ($customer)
+			<img src="{{ asset('storage/' . $customer->customer_logo) }}" alt="Company logo"  />
+		@endif
+        <h1> DETAIL TRANSAKSI KOTOR</h1>
+    </div>
+
+    <!-- CUSTOMER INFO -->
+    <div class="invoice-info">
+        <h2>Customer: {{ strtoupper($customer->customer_nama ?? '') }}</h2>
+        <p>Tanggal: {{ formatDate($model->kotor_tanggal) }}</p>
+        <p>Code: {{ $model->kotor_code ?? null }}</p>
+    </div>
+
+    <!-- TABLE -->
+    <div class="table-responsive">
+        <table>
+            <thead>
+                <tr>
+                    <th style="width:10%;">No.</th>
+                    <th class="text-left" style="width:70%;">Nama Jenis Linen</th>
+                    <th style="width:20%;">Qty</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($data as $table)
+					<tr class="item {{ $loop->last ? 'last' : '' }}">
+						<td>{{ $loop->iteration }}</td>
+						<td class="text-left">{{ $table->jenis_nama }}</td>
+						<td>{{ $table->field_qty }}</td>
+					</tr>
+				@empty
+					<tr class="item last">
+						<td colspan="3">No data available</td>
+					</tr>
+				@endforelse
+            </tbody>
+			<tfoot>
+				<tr>
+					<td colspan="2" style="text-align: right">Total</td>
+					<td>{{ $data->sum('bkotor_qty') }}</td>
+				</tr>
+			</tfoot>
+        </table>
+    </div>
+
+	 <div class="header-action">
         <nav>
             <x-action-print :report-name="'Laporan Kotor'" />
         </nav>
     </div>
 
-    <table border="0" class="header" width="100%">
 
-        <tr>
-            <td></td>
-            <td colspan="10">
-                @if ($customer)
-                    <img style="height:60px;top:5px;" class="logo"
-                        src="{{ asset('storage/' . $customer->customer_logo) }}" alt="logo">
-                @endif
-            </td>
-        </tr>
+</div>
 
-        <tr>
-            <td></td>
-            <td colspan="10">
-                <h2>
-                    <b>DETAIL TRANSAKSI KOTOR </b>
-                </h2>
-            </td>
-        </tr>
-        <tr>
-            <td></td>
-            <td colspan="10">
-                <h3>
-                    CUSTOMER : {{ strtoupper($customer->customer_nama ?? '') }}
-                </h3>
-            </td>
-        </tr>
-        <tr>
-            <td></td>
-            <td colspan="10">
-                <h3>
-                    Periode : {{ formatDate($model->kotor_tanggal) }}
-                </h3>
-            </td>
-        </tr>
-    </table>
+</body>
+</html>
 
-    <h6 style="font-weight: bold;font-size:0.8rem;margin-bottom:5px">
-        KODE : {{ $model->kotor_code ?? null }}
-    </h6>
-
-    <div class="table-responsive" id="table_data">
-
-        <table id="export" border="1" style="border-collapse: collapse !important; border-spacing: 0 !important; width: 100%;"
-            class="table table-bordered table-striped table-responsive-stack">
-            <thead>
-                <tr>
-                    <th style="width: 5%;">No. </th>
-                    <th style="width: 70%;">NAMA JENIS LINEN</th>
-                    <th style="width: 10%;">QTY</th>
-                </tr>
-            </thead>
-            <tbody>
-                @php
-                    $total_berat = 0;
-                @endphp
-
-                @forelse($data as $table)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $table->jenis_nama }}</td>
-                        <td>{{ $table->field_qty }}</td>
-                    </tr>
-                @empty
-                @endforelse
-
-            </tbody>
-        </table>
-    </div>
-
-
-    <table class="footer">
-        <tr>
-            <td colspan="2" class="print-date">{{ $customer->customer_alamat ?? '' }}, {{ date('d F Y') }}</td>
-        </tr>
-        <tr>
-            <td colspan="2" class="print-person">{{ auth()->user()->name ?? '' }}</td>
-        </tr>
-    </table>
 
 </x-report>
